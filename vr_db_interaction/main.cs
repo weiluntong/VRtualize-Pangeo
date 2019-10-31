e0b0d1d6df30a84ca9523eddea8f12c45620ea46
Egermier, Isaac <isaac.egermier@mines.sdsmt.edu>
2019-10-31 16:35:13 -0600

    Initial C# Database script. Requires MySQL NuGet package

---
+++ vr_db_interaction/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vr_db_interaction
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            DBConnection currDBConnection = new DBConnection();
+            currDBConnection.printYeet();
+            currDBConnection.open();
+        }
+    }
+}
